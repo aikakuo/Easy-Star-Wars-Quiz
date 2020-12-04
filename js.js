2fab88e4a85cdad399849a8767d3bb11a3e03a0f
Aika Darling <zh.aikerim@gmail.com>
2020-12-04 02:16:42 -0600

    added score

--- js.js
+++ js.js
@@ -1,33 +1,33 @@
 var startBtn = document.querySelector('.start_btn')
 var rulesBox = document.querySelector('.rules_b')
-var quizBox = document.querySelector('.quizb')
-var nextBtn = document.querySelectorAll('.next')
+var nextBtn = document.querySelectorAll('.buttons .next')
 var queBox = document.querySelector('.question_box')
-
-// console.log(rulesBox)
+var exitBtn = document.querySelector('.buttons .quit_game')
 
 startBtn.addEventListener( "click", function(){
    rulesBox.classList.add("active_rules")
    quizBox.classList.add("active_rules")
 })
 
-nextBtn.addEventListener("click", function() { 
-   queBox.classList.add("active_questions")
-})
+nextBtn.addEventListener( "click", function(){
+       queBox.classList.add("active")
+    //    quizBox.classList.add("active_rules")
+    })
 
-// // time to count down
 
-var div = document.getElementById('set-timer')
-var count = 60
+// // // time to count down
 
-setInterval(function() {
-    count--
-    div.innerText = count + ' seconds have passed'
-    var timesup = 0
-    if(count==timesup){
-        return count;
-    }
-}, 1000) 
+// var div = document.getElementById('set-timer')
+// var count = 60
+
+// setInterval(function() {
+//     count--
+//     div.innerText = count + ' seconds have passed'
+//     var timesup = 0
+//     if(count==timesup){
+//         return count;
+// //     }
+// }, 1000) 
 
 
 
